fac9801e61909aefc87aff00bfae7a728869707f
EtamarRomano <68855892+EtamarRomano@users.noreply.github.com>
2021-03-15 00:25:14 +0200

    Add files via upload

---
+++ B20_Ex02_Main/Board.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex02_MemoryGame
+{
+    internal class Board
+    {
+        private readonly byte m_BoardWidth;
+        private readonly byte m_BoardHight;
+        private char[,] m_GameBoard;
+        private char[,] m_CurrentGameStateBoard;
+        private char[,] m_ComputerMemory;
+
+        internal Board(byte i_boardWidth, byte i_boardHight)
+        {
+            m_BoardWidth = i_boardWidth;
+            m_BoardHight = i_boardHight;
+            m_GameBoard = new char[m_BoardHight, m_BoardWidth];
+            m_CurrentGameStateBoard = new char[m_BoardHight, m_BoardWidth];
+            m_ComputerMemory = new char[m_BoardHight, m_BoardWidth];
+            InitializeBoards();
+        }
+
+        internal byte Hight
+        {
+            get { return m_BoardHight; }
+        }
+
+        internal byte Width
+        {
+            get { return m_BoardWidth; }
+        }
+
+        internal char[,] ComputerMemory
+        {
+            get { return m_ComputerMemory; }
+        }
+
+        internal static bool isValidBoardSize(string i_boardSize, out byte boardSize)
+        {
+            bool ans = true;
+            if (i_boardSize != "4" && i_boardSize != "6")
+            {
+                boardSize = 0;
+                return false;
+            }
+
+            bool parsed = byte.TryParse(i_boardSize, out boardSize);
+            if (!parsed)
+            {
+                ans = false;
+            }
+
+            return ans;
+        }
+        private void InitializeBoards()
+        {
+            for (int i = 0; i < m_BoardHight; i++)
+            {
+                for (int j = 0; j < m_BoardWidth; j++)
+                {
+                    m_CurrentGameStateBoard[i, j] = ' ';
+                    m_ComputerMemory[i, j] = ' ';
+                }
+            }
+
+            List<char> cards = new List<char>();
+            char cardValue = 'A';
+            byte cardIndex = 0;
+            while (cardIndex < m_BoardWidth * m_BoardHight)
+            {
+                cards.Add(cardValue);
+                cards.Add(cardValue);
+                cardValue++;
+                cardIndex += 2;
+            }
+
+            Random cardRand = new Random();
+            byte random_Location = 0;
+            for (int i = 0; i < m_BoardHight; i++)
+            {
+                for (int j = 0; j < m_BoardWidth; j++)
+                {
+                    random_Location = (byte)cardRand.Next(cards.Count);
+                    m_GameBoard[i, j] = cards[random_Location];
+                    cards.RemoveAt(random_Location);
+                }
+            }
+        }
+
+        
+
+        internal void PrintBoard()
+        {
+            char columnLetter = 'A';
+            StringBuilder seperator = new StringBuilder();
+            StringBuilder boardToPrint = new StringBuilder();
+            boardToPrint.Append("    ");
+            seperator.Append("  =");
+            string boardCell;
+            for (int i = 0; i < m_BoardWidth; i++)
+            {
+                seperator.Append("====");
+                boardToPrint.Append(columnLetter + "   ");
+                columnLetter++;
+            }
+
+            for (int i = 0; i < m_BoardHight; i++)
+            {
+                boardToPrint.Append("\n");
+                boardToPrint.Append(seperator);
+                boardToPrint.Append("\n");
+                boardToPrint.Append((i + 1) + " |");
+                for (int j = 0; j < m_BoardWidth; j++)
+                {
+                    boardCell = string.Format(" {0} |", m_CurrentGameStateBoard[i, j]);
+                    boardToPrint.Append(boardCell);
+                }
+            }
+
+            boardToPrint.Append("\n");
+            boardToPrint.Append(seperator);
+            Console.WriteLine(boardToPrint.ToString());
+        }
+
+        internal void ShowBoard(string i_Slot1 = null, string i_Slot2 = null)
+        {
+            if (i_Slot1 == null && i_Slot2 == null)
+            {
+                PrintBoard();
+            }
+
+            if (i_Slot1 != null && i_Slot2 == null)
+            {
+                byte[] index = SlotToIndex(i_Slot1);
+                m_CurrentGameStateBoard[index[0], index[1]] = m_GameBoard[index[0], index[1]];
+                m_ComputerMemory[index[0], index[1]] = m_GameBoard[index[0], index[1]];
+                PrintBoard();
+            }
+
+            if (i_Slot1 != null && i_Slot2 != null)
+            {
+                byte[] index1 = SlotToIndex(i_Slot1);
+                byte[] index2 = SlotToIndex(i_Slot2);
+                m_CurrentGameStateBoard[index1[0], index1[1]] = m_GameBoard[index1[0], index1[1]];
+                m_CurrentGameStateBoard[index2[0], index2[1]] = m_GameBoard[index2[0], index2[1]];
+                m_ComputerMemory[index2[0], index2[1]] = m_GameBoard[index2[0], index2[1]];
+                PrintBoard();
+                if (!IsGussedCorrectly(i_Slot1, i_Slot2))
+                {
+                    m_CurrentGameStateBoard[index1[0], index1[1]] = ' ';
+                    m_CurrentGameStateBoard[index2[0], index2[1]] = ' ';
+                }
+            }
+        }
+
+        internal bool IsValidSlot(string i_Slot, char mode)
+        {
+            bool ans = true;
+            byte[] index = new byte[2];
+            if (i_Slot.Equals(string.Empty) || (i_Slot.Length != 2 && !i_Slot.Equals("Q")))
+            {
+                if (mode == 'p')
+                {
+                    Console.WriteLine("please enter a valid slot like A1, B2 etc...");
+                }
+
+                return false;
+            }
+
+            if (i_Slot == "Q")
+            {
+                return true;
+            }
+
+            if (ans == true && (i_Slot[0] < 'A' || i_Slot[0] > 'Z') || (i_Slot[1] < '0' || i_Slot[1] > '9'))
+            {
+                ans = false;
+                if (mode == 'p')
+                {
+                    Console.WriteLine("please enter a valid slot like A1, B2 etc...");
+                }
+            }
+
+            if (ans == true && !i_Slot.Equals("Q") && ((i_Slot[0] < 'A' || i_Slot[0] > ('A' + m_BoardWidth - 1)) || (i_Slot[1] < '1' || i_Slot[1] > ('1' + m_BoardHight - 1))))
+            {
+                ans = false;
+                if (mode == 'p')
+                {
+                    Console.WriteLine("the slot you entered is out of bounds, please try again");
+                }
+            }
+
+            index = SlotToIndex(i_Slot);
+            if (ans == true && !i_Slot[0].Equals("Q") && m_CurrentGameStateBoard[index[0], index[1]] != ' ')
+            {
+                ans = false;
+                if (mode == 'p')
+                {
+                    Console.WriteLine("this slot is not valid beacuse its is already fliped, please try again");
+                }
+            }
+
+            return ans;
+        }
+
+        internal bool IsGussedCorrectly(string i_Slot1, string i_Slot2) // מחזיר 0 או 1 ומדפיס כל הכבוד בהתאם ומוחק בסוף את המסך
+        {
+            bool ans = false;
+            byte[] index1 = new byte[2];
+            byte[] index2 = new byte[2];
+            index1 = SlotToIndex(i_Slot1);
+            index2 = SlotToIndex(i_Slot2);
+            if (m_GameBoard[index1[0], index1[1]] == m_GameBoard[index2[0], index2[1]])
+            {
+                ans = true;
+                m_CurrentGameStateBoard[index1[0], index1[1]] = m_GameBoard[index1[0], index1[1]];
+                m_CurrentGameStateBoard[index2[0], index2[1]] = m_GameBoard[index2[0], index2[1]];
+            }
+
+            return ans;
+        }
+
+        internal byte[] SlotToIndex(string i_Slot)
+        {
+            byte[] index = { (byte)(byte.Parse(i_Slot[1].ToString()) - 1), (byte)((char)(i_Slot[0]) - 65) };
+            return index;
+        }
+
+        internal string IndexToSlot(int row, int colum)
+        {
+            string Slot;
+            Slot = (char)(colum + 65) + (row + 1).ToString();
+            Console.WriteLine(Slot);
+            return Slot;
+        }
+
+        internal bool IsGameFinished(byte i_Player1Score, byte i_Player2Score)
+        {
+            bool ans = false;
+            if (((i_Player1Score + i_Player2Score) * 2) == m_BoardHight * m_BoardWidth)
+            {
+                ans = true;
+            }
+
+            return ans;
+        }
+
+        internal bool IsGameTerminated(string i_chosenWord)
+        {
+            bool ans = false;
+            if (i_chosenWord == "Q")
+            {
+                ans = true;
+                Console.WriteLine("Goodbye, See you next time!");
+                System.Threading.Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ B20_Ex02_Main/GamePlay.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B20_Ex02_MemoryGame;
+
+namespace B20_Ex02_MemoryGame
+{
+    internal class GamePlay
+    {
+        private string m_mode;
+        private Board m_board;
+        private Player m_player1;
+        private Player m_player2;
+
+        internal GamePlay(string i_mode, Board i_board, Player i_player1, Player i_player2)
+        {
+            this.m_mode = i_mode;
+            this.m_board = i_board;
+            this.m_player1 = i_player1;
+            this.m_player2 = i_player2;
+        }
+
+        internal void StartGame()
+        {
+            if (m_mode == "c")
+            {
+                CompVsPlayerGamePlay();
+            }
+            else
+            {
+                PlayerVsPlayerGameplay();
+            }
+
+            m_board.ShowBoard();
+            Console.WriteLine("{0}'s score is: " + m_player1.Score, m_player1.Name);
+            Console.WriteLine("{0}'s score is: " + m_player2.Score, m_player2.Name);
+            if (m_player2.Score > m_player1.Score)
+            {
+                Console.WriteLine("{0} won the Game, Congratulations!", m_player2.Name);
+            }
+
+            if (m_player2.Score < m_player1.Score)
+            {
+                Console.WriteLine("{0} won the Game, Congratulations!", m_player1.Name);
+            }
+
+            if (m_player2.Score == m_player1.Score)
+            {
+                Console.WriteLine("its a draw!!!");
+            }
+        }
+
+        private void CompVsPlayerGamePlay()
+        {
+            m_player2 = new Player("Comp");
+            Random rand = new Random();
+            byte turn_Flag = (byte)rand.Next(2); // determines who starts the game
+            string chosen_Card_1 = string.Empty;
+            string chosen_Card_2 = string.Empty;
+
+            Console.WriteLine("Hello {0}, we are ready to start", m_player1.Name);
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(@"Game Instructions:
+in each turn any player needs to choose a two slots on the game board to reveal,
+if they are the same you'll get 1 point otherwise the slots will return to hidden state and the i get the turn.
+pay attention to pick a proper slot in the board game like A1/E3 etc... (use capital letters)");
+            Console.WriteLine("Press enter if you are ready to Start!");
+            Console.ReadLine();
+            Ex02.ConsoleUtils.Screen.Clear();
+            while (!m_board.IsGameFinished(m_player1.Score, m_player2.Score))
+            {
+                if (turn_Flag == 1)
+                {
+                    Console.WriteLine("{0} it your turn to play!", m_player1.Name);
+                    m_board.ShowBoard();
+                    Console.WriteLine("please enter a slot to reveal");
+                    chosen_Card_1 = Console.ReadLine();
+                    /// checking argument validation
+                    while (!m_board.IsValidSlot(chosen_Card_1, 'p'))
+                    {
+                        chosen_Card_1 = Console.ReadLine();
+                    }
+
+                    /// end of validation checking
+                    m_board.IsGameTerminated(chosen_Card_1); /// system shuts down in case of "Q"
+
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    m_board.ShowBoard(chosen_Card_1);
+                    Console.WriteLine("please enter another slot to reveal");
+                    chosen_Card_2 = Console.ReadLine();
+                    /// checking argument validation
+                    while (!m_board.IsValidSlot(chosen_Card_2, 'p'))
+                    {
+                        chosen_Card_2 = Console.ReadLine();
+                    }
+
+                    /// end of validation checking                    
+                    m_board.IsGameTerminated(chosen_Card_1); /// system shuts down in case of "Q"
+
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    m_board.ShowBoard(chosen_Card_1, chosen_Card_2);
+                    System.Threading.Thread.Sleep(2000);
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    if( m_board.IsGussedCorrectly(chosen_Card_1, chosen_Card_2))
+                    {
+                        m_player1.Score++;
+                    }
+                    else
+                    {
+                        turn_Flag = (byte)(1 - turn_Flag); // making the turn to switch to the other player
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("It's my turn now");
+                    System.Threading.Thread.Sleep(1000);
+                    m_board.ShowBoard();
+                    System.Threading.Thread.Sleep(1000);
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    //// Choosing first random slot
+                    chosen_Card_1 = m_player2.ComputerFirstMove(m_board);
+                    while (!m_board.IsValidSlot(chosen_Card_1, 'c'))
+                    {
+                        chosen_Card_1 = m_player2.ComputerFirstMove(m_board);
+                    }
+                    //// Slot has been chosen
+
+                    m_board.ShowBoard(chosen_Card_1);
+                    //// Choosing second random slot
+                    chosen_Card_2 = m_player2.ComputerSecondMove(m_board, chosen_Card_1);
+                    while (!m_board.IsValidSlot(chosen_Card_2, 'c'))
+                    {
+                        chosen_Card_2 = m_player2.ComputerSecondMove(m_board, chosen_Card_1);
+                    }
+                    //// Slot has been chosen
+
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    m_board.ShowBoard(chosen_Card_1, chosen_Card_2);
+                    System.Threading.Thread.Sleep(2000);
+                    Ex02.ConsoleUtils.Screen.Clear();
+                    if(m_board.IsGussedCorrectly(chosen_Card_1, chosen_Card_2))
+                    {
+                        m_player2.Score++;
+                    }
+                    else
+                    {
+                        turn_Flag = (byte)(1 - turn_Flag); /// making the turn to switch to the other player
+                    }
+
+                }
+
+                chosen_Card_1 = string.Empty;
+                chosen_Card_2 = string.Empty;
+            }
+        }
+
+        private void PlayerVsPlayerGameplay()
+        {
+            Random rand = new Random();
+            byte turn_Flag = (byte)rand.Next(2); // determines who starts the game
+            string chosen_Card_1 = null;
+            string chosen_Card_2 = null;
+
+            Console.WriteLine("Hello {0} and {1}, we are ready to start", m_player1.Name, m_player2.Name);
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(@"Game Instructions:
+in each turn any player needs to choose a two slots on the game board to reveal,
+if they are the same you'll get 1 point otherwise the slots will return to hidden state and the other player gets the turn.
+pay attention to pick a proper slot in the board game like A1/E3 etc... (use capital letters)");
+            Console.WriteLine("Press enter if you are ready to Start!");
+            Console.ReadLine();
+            Ex02.ConsoleUtils.Screen.Clear();
+            while (!m_board.IsGameFinished(m_player1.Score, m_player2.Score))
+            {
+                Console.WriteLine("{0} it your turn to play!", turn_Flag == 1 ? m_player1.Name : m_player2.Name);
+                m_board.ShowBoard();
+                Console.WriteLine("please enter a slot to reveal");
+                chosen_Card_1 = Console.ReadLine();
+                /// checking argument validation
+                while (!m_board.IsValidSlot(chosen_Card_1, 'p'))
+                {
+                    chosen_Card_1 = Console.ReadLine();
+                }
+
+                m_board.IsGameTerminated(chosen_Card_1); /// system shuts down in case of "Q"
+                                                         /// end of validation checking
+                Ex02.ConsoleUtils.Screen.Clear();
+                m_board.ShowBoard(chosen_Card_1);
+                Console.WriteLine("please enter another slot to reveal");
+                chosen_Card_2 = Console.ReadLine();
+                /// checking argument validation
+                while (!m_board.IsValidSlot(chosen_Card_2, 'p'))
+                {
+                    chosen_Card_2 = Console.ReadLine();
+                }
+
+                m_board.IsGameTerminated(chosen_Card_1); /// system shuts down in case of "Q"
+                /// end of validation checking
+                Ex02.ConsoleUtils.Screen.Clear();
+                m_board.ShowBoard(chosen_Card_1, chosen_Card_2);
+                System.Threading.Thread.Sleep(2000);
+                Ex02.ConsoleUtils.Screen.Clear();
+                if(m_board.IsGussedCorrectly(chosen_Card_1, chosen_Card_2) && turn_Flag == 1)
+                {
+                    m_player1.Score++;
+                }
+                else if (m_board.IsGussedCorrectly(chosen_Card_1, chosen_Card_2) && turn_Flag == 0)
+                {
+                    m_player2.Score++;
+                }
+                else
+                {
+                    turn_Flag = (byte)(1 - turn_Flag); // making the turn to switch to the other player
+                }
+            }
+        }
+    }
+}
